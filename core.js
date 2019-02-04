903e021df6d117da8109d6ca9efaa1baa5240742
VenoDev <47304933+VenoDev@users.noreply.github.com>
2019-02-04 20:07:40 +0100

    RIP

--- core.js
+++ core.js
@@ -1,4 +1,5 @@
 //Welcome to core of this lang
+//This script will probably never be used in any part of this project. V_V
 alert("Remember that work is in progress! /n SCode made by VenoDev/Erixo /n published at MIT license"); // slash n is ENTER inside alerts, prompts and confirms
 
 var readingLine = 1;
