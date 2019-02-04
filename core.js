7f72c6e838a4c03b51f1f21357cc9a6308774168
VenoDev <47304933+VenoDev@users.noreply.github.com>
2019-02-04 19:59:30 +0100

    Update core.js

--- core.js
+++ core.js
@@ -1,5 +1,5 @@
 //Welcome to core of this lang
-alert("Remember that work is in progress! /n SCode made by VenoDev/Erixo /n published as MIT license"); // slash n is ENTER inside alerts, prompts and confirms
+alert("Remember that work is in progress! /n SCode made by VenoDev/Erixo /n published at MIT license"); // slash n is ENTER inside alerts, prompts and confirms
 
 var readingLine = 1;
 var readingLetter = "";
@@ -10,7 +10,7 @@ function start()
     readingLine = "";
     readingLetter = "";
     readingLineString = "";
-    while(readingLine < editor.)
+    while(readingLine < Editor.scriptEditor)
     {
         
     }
