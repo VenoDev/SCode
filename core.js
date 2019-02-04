244a14f5415ddfc2226675324f997b7b96f213d5
VenoDev <47304933+VenoDev@users.noreply.github.com>
2019-02-04 17:56:37 +0100

    added stuff

--- core.js
+++ core.js
@@ -1,10 +1,19 @@
 //Welcome to core of this lang
-alert("Remember that work is in progress! /n SCode made by VenoDev/Erixo /n published as MIT license") // slash n is ENTER inside alerts, prompts and confirms
+alert("Remember that work is in progress! /n SCode made by VenoDev/Erixo /n published as MIT license"); // slash n is ENTER inside alerts, prompts and confirms
 
 var readingLine = 1;
+var readingLetter = "";
+var readingString = "";
 
 function interpretate()
 {
-    <script src="interpretator.js"></script>
+    readingLine = "";
+    readingLetter = "";
+    readingLineString = "";
+    while( == true)
+    {
+        
+    }
+    
         //idk is this working? ^^
 }
